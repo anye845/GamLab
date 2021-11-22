e6e87e620b3e51aaec953c997d5b4a01802c34d5
yasudananami <nyasu001@plattsburgh.edu>
2021-11-22 14:58:28 -0500

    idk what im doing

---
+++ app/src/main/java/com/example/gamlab/TimeSlots.java
@@ -0,0 +1,21 @@
+package com.example.gamlab;
+
+public class TimeSlots {
+    private String time;
+
+
+    public TimeSlots(){}
+
+    public TimeSlots(String time) {
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+}
