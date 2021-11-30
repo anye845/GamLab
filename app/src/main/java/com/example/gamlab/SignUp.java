e6474bdd7713b15d7599bf3d8d9aae422d5625ba
yasudananami <nyasu001@plattsburgh.edu>
2021-11-30 12:27:29 -0500

    idk what im doing

--- app/src/main/java/com/example/gamlab/SignUp.java
+++ app/src/main/java/com/example/gamlab/SignUp.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,18 +16,30 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
+import com.google.api.LogDescriptor;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SignUp extends AppCompatActivity {
+    public static final String TAG = "TAG";
     EditText etFirstName, etLastName, etEmail, etPassword;
     Button etBtnRegister;
     Button btnLoginReg;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +54,7 @@ public class SignUp extends AppCompatActivity {
         btnLoginReg = findViewById(R.id.btnLogin);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
         if (fAuth.getCurrentUser() != null) {
@@ -50,8 +65,10 @@ public class SignUp extends AppCompatActivity {
         etBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = etEmail.getText().toString().trim();
-                String password = etPassword.getText().toString().trim();
+                final String email = etEmail.getText().toString().trim();
+                final String password = etPassword.getText().toString().trim();
+                final String firstName = etFirstName.getText().toString();
+                final String lastName = etLastName.getText().toString();
 
                 if (TextUtils.isEmpty(email)) {
                     etEmail.setError("Email is required");
@@ -68,6 +85,7 @@ public class SignUp extends AppCompatActivity {
                     etPassword.setError("Password Must be more than 6 characters");
                     return;
                 }
+
                 progressBar.setVisibility(View.VISIBLE);
 
                 // register the user in firebase
@@ -77,17 +95,34 @@ public class SignUp extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(SignUp.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fName",firstName);
+                            user.put("lName",lastName);
+                            user.put("email",email);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: user profile is created for " + userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: " + e.toString());
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
                         }else {
-                                Toast.makeText(SignUp.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignUp.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
 
                     }
 
-                    });
-                }
-            });
+                });
+            }
+        });
 
         btnLoginReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,5 +130,5 @@ public class SignUp extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), LogIn1.class));
             }
         });
-        }
     }
+}
